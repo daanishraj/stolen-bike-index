f09db750c48ecb4e7c64ba43296cdd2a2eb0c9c4
Daanish Raj <daanish.raj@metiundo.io>
2024-05-07 10:32:10 +0200

    temporary changes to fetch entire dataset on front end

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -2,21 +2,52 @@ import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { Button, Pagination, Flex, TextInput, rem } from '@mantine/core';
 import { IconFilter } from '@tabler/icons-react';
-import { BikeCountGetResponse, BikeSearchGetResponse } from '@/types/types';
+import axios from 'axios';
+import { Bike, BikeCountGetResponse, BikeSearchGetResponse } from '@/types/types';
 import BikesTable from '../components/bikes-table';
 import styles from './index.module.css';
 import useGetBikes from '../hooks/use-get-bikes';
 import useGetBikesCount from '../hooks/use-get-bikes-count';
 
 const Search = () => {
+    const [isCompleted, setIsCompleted] = React.useState(false);
+    const [completeData, setCompleteData] = React.useState<BikeSearchGetResponse['bikes']>([]);
     const [bikeSearchData, setBikeSearchData] = React.useState<BikeSearchGetResponse['bikes']>([]);
     const [bikeCountData, setBikeCountData] = React.useState<Partial<BikeCountGetResponse>>({});
     const [page, setPage] = React.useState(1);
-    const [filterByTitleText, setFilterByTitleText] = React.useState('');
+    const [searchText, setSearchText] = React.useState('');
     const [searchParams, setSearchParams] = useSearchParams();
     const { searchData, isSearching, isSearchingError, isRefetchingSearch, refetchSearch } =
     useGetBikes(searchParams);
     const { countData, isCounting, isCountingError } = useGetBikesCount();
+
+    React.useEffect(() => {
+        //when the component mounts, scrape all the data from the bikeindex api
+        const url = 'https://bikeindex.org/api/v3/search';
+        const per_page = '100';
+        // eslint-disable-next-line @typescript-eslint/no-shadow
+        const getDataForPage = async (page: number) : Promise<BikeSearchGetResponse> => {
+            const params = {
+                page: `${page}`,
+                per_page,
+            };
+            const response = await axios.get(url, { params });
+            return response.data;
+        };
+        const getData = async () => {
+            // eslint-disable-next-line no-plusplus
+            for (let i = 0; i < 41; i++) {
+                const data = await getDataForPage(i);
+                console.log(`page ${i}`);
+                setCompleteData(prevData => [...prevData, ...data.bikes]);
+                }
+        };
+        getData();
+        console.log('fetched all pages');
+        setIsCompleted(true);
+    }, []);
+
+    console.log('complete data: ', completeData.length);
 
 React.useEffect(() => {
     if (searchData) {
@@ -48,24 +79,27 @@ const handleClickAll = () => {
     setSearchParams(updatedSearchParams);
 };
 
-const onFilterByTitle = (value: string) => {
-    setFilterByTitleText(value);
-    if (!value) {
-        const updatedSearchParams = new URLSearchParams(searchParams);
-        updatedSearchParams.has('query') && updatedSearchParams.delete('query');
-        setSearchParams(updatedSearchParams);
-    }
+const onSearchByText = (value: string) => {
+    setSearchText(value);
+    // if (!value) {
+    //     const updatedSearchParams = new URLSearchParams(searchParams);
+    //     updatedSearchParams.has('query') && updatedSearchParams.delete('query');
+    //     setSearchParams(updatedSearchParams);
+    // }
 };
 
 const onFilter = () => {
-    const updatedSearchParams = new URLSearchParams(searchParams);
-    if (!filterByTitleText) {
-        updatedSearchParams.has('query') && updatedSearchParams.delete('query');
-        setSearchParams(updatedSearchParams);
-        return;
-    }
-    updatedSearchParams.set('query', filterByTitleText);
-    setSearchParams(updatedSearchParams);
+    const filteredData = completeData.filter((bike: Bike) =>
+        bike.title?.toLowerCase().includes(searchText.toLowerCase()));
+    setCompleteData(filteredData);
+    // const updatedSearchParams = new URLSearchParams(searchParams);
+    // if (!filterByTitleText) {
+    //     updatedSearchParams.has('query') && updatedSearchParams.delete('query');
+    //     setSearchParams(updatedSearchParams);
+    //     return;
+    // }
+    // updatedSearchParams.set('query', filterByTitleText);
+    // setSearchParams(updatedSearchParams);
 };
 
   const onPageChange = ((pageNum: number) => {
@@ -92,8 +126,8 @@ const getHeaderContent = () => {
         <Flex>
             <TextInput
               placeholder="search by text.."
-              onChange={(event) => onFilterByTitle(event?.currentTarget.value)}
-              value={filterByTitleText}
+              onChange={(event) => onSearchByText(event?.currentTarget.value)}
+              value={searchText}
               radius="lg"
           />
             <Button onClick={onFilter} color="dark"><IconFilter style={{ width: rem(14), height: rem(14) }} /></Button>
@@ -116,8 +150,9 @@ const getHeaderContent = () => {
             <Button onClick={handleClickAll} color="dark">Stolen everywhere</Button>
             </Flex>
             <div className={styles.tableContainer}>
-            {bikeSearchData.length === 0 && (<div> there is no data to display</div>)}
-                <BikesTable bikes={bikeSearchData} />
+            {completeData.length === 0 && (<div> there is no data to display</div>)}
+                {/* <BikesTable bikes={bikeSearchData} /> */}
+                <BikesTable bikes={completeData} />
             </div>
             </>
         );
